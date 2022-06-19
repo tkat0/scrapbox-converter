2483c2490263caef0306cc53af63920ffe623f75
tkat0 <tkato.dev@gmail.com>
2022-06-19 14:48:08 +0900

    format

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import init, { hex_color_js } from 'scrapbox-parser-core';
+import init, { hex_color_js } from "scrapbox-parser-core";
 
 const main = async () => {
-    await init();
-    console.log(hex_color_js('#2F14DF'));
+  await init();
+  console.log(hex_color_js("#2F14DF"));
 };
 
 main();
