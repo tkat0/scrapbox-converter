eac0bcd54761e965181d75a06c37103966b5a1f2
tkat0 <tkato.dev@gmail.com>
2022-08-06 08:14:30 +0900

    feat: Support obsidian image internal link ![img.png]]

--- src/parser/markdown.rs
+++ src/parser/markdown.rs
@@ -51,6 +51,7 @@ fn node(input: Span) -> IResult<Node> {
         map(external_link, |c| Node::new(NodeKind::ExternalLink(c))),
         map(math, |c| Node::new(NodeKind::Math(c))),
         // NOTE(tkat0): keep internal_link at the bottom of parsing bracket node
+        map(image_internal_link, |c| Node::new(NodeKind::Image(c))),
         map(internal_link, |c| Node::new(NodeKind::InternalLink(c))),
         map(external_link_plain, |s| {
             Node::new(NodeKind::ExternalLink(s))
@@ -73,6 +74,24 @@ fn external_link(input: Span) -> IResult<ExternalLink> {
     let (input, url) = parentheses(input)?;
 
     Ok((input, ExternalLink::new(Some(*title), *url)))
+}
+
+/// Obsidian image internal link
+/// ![[test.png]]
+fn image_internal_link(input: Span) -> IResult<Image> {
+    let (input, url) = delimited(tag("![["), take_while(|c| c != ']'), tag("]]"))(input)?;
+
+    let ext = ["svg", "jpg", "jpeg", "png", "gif"];
+    let is_image = |url: &str| ext.iter().any(|e| url.ends_with(e));
+
+    if is_image(*url) {
+        return Ok((input, Image::new(*url)));
+    } else {
+        Err(Err::Error(ParseError::new(
+            input,
+            "URL is not image".into(),
+        )))
+    }
 }
 
 /// ![](https://i.gyazo.com/da78df293f9e83a74b5402411e2f2e01.png)
@@ -286,6 +305,16 @@ mod test {
     fn internal_link_valid_test(input: &str, expected: (&str, InternalLink)) {
         assert_eq!(
             internal_link(Span::new(input)).map(|(input, ret)| (*input, ret)),
+            Ok(expected)
+        );
+    }
+
+    #[rstest(input, expected,
+        case("![[test.png]]", ("", Image::new("test.png"))),
+    )]
+    fn image_internal_link_valid_test(input: &str, expected: (&str, Image)) {
+        assert_eq!(
+            image_internal_link(Span::new(input)).map(|(input, ret)| (*input, ret)),
             Ok(expected)
         );
     }
