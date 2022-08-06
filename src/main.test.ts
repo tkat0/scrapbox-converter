80a5eb09fa8e838352620d741d1052e88e612a6b
tkat0 <tkato.dev@gmail.com>
2022-08-06 23:24:33 +0900

    fix: e2e test

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,6 +1,7 @@
-import { vi, describe, it, expect, beforeAll } from "vitest";
+import { vi, describe, expect, beforeAll, test } from "vitest";
 import { defaultConfig } from "./app/ConfigModal";
 import { init, scrapboxToMarkdown } from "./main";
+import fs from "fs";
 
 vi.stubGlobal("fetch", await import("fs").then((mod) => mod.readFileSync));
 
@@ -9,9 +10,15 @@ describe(`scrapboxToMarkdown`, () => {
     await init();
   });
 
-  it(`should convert a internal link`, () => {
-    expect(scrapboxToMarkdown(`[internal-link]`, defaultConfig)).toEqual(
-      `[[internal-link]]`
+  test(`Scrapboxの使い方`, () => {
+    const input = fs.readFileSync(
+      "./src/__test__/help-jp/Scrapboxの使い方_input.txt",
+      "utf8"
     );
+    const expected = fs.readFileSync(
+      "./src/__test__/help-jp/Scrapboxの使い方_expected.txt",
+      "utf8"
+    );
+    expect(scrapboxToMarkdown(input, defaultConfig)).toEqual(expected);
   });
 });
